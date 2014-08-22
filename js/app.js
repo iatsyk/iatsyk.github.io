0dfbb8c1d80771d2a6197e7a1cb9db057304d76f
Vova Iatsyk <yatsyk.ve@gmail.com>
2014-08-22 12:35:58 +0300

    discard angular

--- js/app.js
+++ js/app.js
@@ -1,22 +1,13 @@
-function get_cookie ( cookie_name ) {
-	var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
-	if ( results )
-		return ( unescape ( results[2] ) );
-	else
-		return null;
+function get_cookie(cookie_name) {
+    var results = document.cookie.match('(^|;) ?' + cookie_name + '=([^;]*)(;|$)');
+    if (results)
+        return ( unescape(results[2]) );
+    else
+        return null;
 }
 
-function delete_cookie ( cookie_name ) {
-	var cookie_date = new Date ( );  // Текущая дата и время
-	cookie_date.setTime ( cookie_date.getTime() - 1 );
-	document.cookie = cookie_name += "=; expires=" + cookie_date.toGMTString();
+function delete_cookie(cookie_name) {
+    var cookie_date = new Date();
+    cookie_date.setTime(cookie_date.getTime() - 1);
+    document.cookie = cookie_name += "=; expires=" + cookie_date.toGMTString();
 }
-
-(function () {
-	var app = angular.module('OSL', ['hour-directives']);
-
-	app.controller('MainController', function () {
-        	this.hour = get_cookie("hour");
-	});
-
-})();
--- js/some.js
+++ js/some.js
@@ -1,14 +1,25 @@
-$(document).ready(function(){
-$("#clickBtn").on("click", function () {
-	console.log("in add");
-	var hour = get_cookie("hour");
-	if (hour == null) {
-		document.cookie = "hour=0;expires=15/08/2014 00:00:00";
-	}
-	hour++;
-	delete_cookie("hour");
-	document.cookie = "hour=" + hour + ";expires=15/08/2014 00:00:00";
-	var message = "hour:  " + hour;
-    	$("#hour").text(message);
-	});
+$(document).ready(function () {
+
+    setHourOnLoadPage();
+
+    $("#clickBtn").on("click", function () {
+        console.log("in add");
+        var hour = get_cookie("hour");
+        hour ++;
+        delete_cookie("hour");
+        document.cookie = "hour=" + hour + ";expires=15/10/2014 00:00:00";
+        var message = "hour:  " + hour;
+        $("#hour").text(message);
+    });
 });
+
+function setHourOnLoadPage() {
+    console.log("in setHourOnLoadPage");
+    var hour = get_cookie("hour");
+    if (hour == null) {
+        document.cookie = "hour=0;expires=15/10/2014 00:00:00";
+        hour = 0;
+    }
+    var message = "hour:  " + hour;
+    $("#hour").text(message);
+}
