5172f4687560ae5c452ada6fb4715cec0e271178
Vova Iatsyk <yatsyk.ve@gmail.com>
2014-08-23 13:12:47 +0300

    add setInterval and clear button

--- js/some.js
+++ js/some.js
@@ -1,16 +1,34 @@
 $(document).ready(function () {
 
     setHourOnLoadPage();
+    setInterval(function () {
+        var hour = incHour();
+        updateHour(hour);
+    }, 1000)
+
 
     $("#clickBtn").on("click", function () {
         console.log("in add");
-        var hour = getCookie("hour");
-        hour ++;
-        setCookie("hour", hour, 60);
-        var message = "hour:  " + hour;
-        $("#hour").text(message);
+        var hour = incHour();
+        updateHour(hour);
+    });
+    $("#clearBtn").on("click", function () {
+        setCookie("hour", 0, 60);
+        updateHour(0);
     });
 });
+
+function updateHour(hour) {
+    var message = "hour:  " + hour;
+    $("#hour").text(message);
+}
+
+function incHour() {
+    var hour = getCookie("hour");
+    hour++;
+    setCookie("hour", hour, 60);
+    return hour;
+}
 
 function setHourOnLoadPage() {
     console.log("in setHourOnLoadPage");
